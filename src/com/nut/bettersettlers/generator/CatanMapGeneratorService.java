afc793153f08272f37bdda9f6b6a8a40ec4b6ba4
Andrew Flynn <aflynn@gmail.com>
2013-09-07 10:11:53 -0700

    BetterSettersMapGenerator v1.0.1
    Move maps to separate class for easier stripping
    Real icon

--- src/com/nut/bettersettlers/generator/CatanMapGeneratorService.java
+++ src/com/nut/bettersettlers/generator/CatanMapGeneratorService.java
@@ -3,7 +3,6 @@ package com.nut.bettersettlers.generator;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 import android.app.IntentService;
@@ -13,36 +12,10 @@ import android.content.res.Resources;
 import android.util.Log;
 
 import com.nut.bettersettlers.generator.data.CatanMapGenerator;
+import com.nut.bettersettlers.generator.data.Maps;
 
 public class CatanMapGeneratorService extends IntentService {
 	private static final String TAG = "CatanMapGenerator";
-	
-	private static final Map<String, Integer> MAPS = new HashMap<String, Integer>();
-	static {
-		MAPS.put("Standard", R.raw.standard);
-		MAPS.put("Large", R.raw.large);
-		MAPS.put("XLarge", R.raw.xlarge);
-		
-		MAPS.put("HeadingForNewShores", R.raw.heading_for_new_shores);
-		MAPS.put("TheFourIslands", R.raw.the_four_islands);
-		MAPS.put("TheFogIsland", R.raw.the_fog_island);
-		MAPS.put("ThroughTheDesert", R.raw.through_the_desert);
-		MAPS.put("TheForgottenTribe", R.raw.the_forgotten_tribe);
-		MAPS.put("ClothForCatan", R.raw.cloth_for_catan);
-		MAPS.put("ThePirateIslands", R.raw.the_pirate_islands);
-		MAPS.put("TheWondersOfCatan", R.raw.the_wonders_of_catan);
-		MAPS.put("NewWorld", R.raw.new_world);
-		
-		MAPS.put("HeadingForNewShoresExp", R.raw.heading_for_new_shores_exp);
-		MAPS.put("TheFourIslandsExp", R.raw.the_four_islands_exp);
-		MAPS.put("TheFogIslandExp", R.raw.the_fog_island_exp);
-		MAPS.put("ThroughTheDesertExp", R.raw.through_the_desert_exp);
-		MAPS.put("TheForgottenTribeExp", R.raw.the_forgotten_tribe_exp);
-		MAPS.put("ClothForCatanExp", R.raw.cloth_for_catan_exp);
-		MAPS.put("ThePirateIslandsExp", R.raw.the_pirate_islands_exp);
-		MAPS.put("TheWondersOfCatanExp", R.raw.the_wonders_of_catan_exp);
-		MAPS.put("NewWorldExp", R.raw.new_world_exp);
-	}
 	
 	public CatanMapGeneratorService() {
 		super(CatanMapGeneratorService.class.getName());
@@ -57,7 +30,8 @@ public class CatanMapGeneratorService extends IntentService {
 		Log.i(TAG, "Start");
 
 		Resources res = getResources();
-		for (Map.Entry<String, Integer> map : MAPS.entrySet()) {
+		for (Map.Entry<String, Integer> map : Maps.MAPS.entrySet()) {
+			
 			write(map.getKey() + ".java", CatanMapGenerator.generateFromJson(res.openRawResource(map.getValue())).toClassString(map.getKey()));
 		}
 	}
--- src/com/nut/bettersettlers/generator/MainActivity.java
+++ src/com/nut/bettersettlers/generator/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		CatanMapGeneratorService.start(this);
+		
+		setResult(RESULT_OK);
+		finish();
 	}
 }
